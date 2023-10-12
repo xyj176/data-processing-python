6bcfbfcd59c8bd3770760e9654a92fbac02a77da
xuyj <xuyj@dist.com.cn>
2023-10-12 10:45:43 +0800

    feat:添加影像裁剪

---
+++ image/clip/clip_main.py
@@ -0,0 +1,21 @@
+import os
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="影像数据裁剪处理")
+    parser.add_argument('data_root', help='影像数据的根目录')
+    args = parser.parse_args()
+    return args
+
+
+def method():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
